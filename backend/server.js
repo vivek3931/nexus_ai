908e6fdc3b03293422fda30d33c0372fb8adaaa0
Vivek Singh <viveks3931@gmail.com>
2025-12-20 22:01:14 +0530

    fixed cors by adding frontend url

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-    origin: ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:3000'],
+    origin: ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:3000' , 'https://nexusai-chat.netlify.app'],
     credentials: true
 }));
 app.use(express.json({ limit: '50mb' }));
